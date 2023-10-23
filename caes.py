619cd54527f831898ce4613cdf24f85e15bb6d1e
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-10-23 15:29:57 -0400

    Update caes.py

--- caes.py
+++ caes.py
@@ -1,12 +1,12 @@
 import streamlit as st
 ##import psycopg2
 from supabase import create_client, Client
-from st-supabase-connection import SupabaseConnection
+##from st-supabase-connection import SupabaseConnection
 
 st.set_page_config(page_title="Caes abrigados")
 def apresenta():
     try:
-        conn = st.experimental_connection("supabase",type=SupabaseConnection)
+        ##conn = st.experimental_connection("supabase",type=SupabaseConnection)
         ##connection = psycopg2.connect(
         ##           host='db.ibhcxtnwnonsnycfgjay.supabase.co',
         ##           user='postgres',
@@ -16,8 +16,8 @@ def apresenta():
         ##)
         st.write("conexao exitosa")
         ##cursor = connection.cursor()
-        rows = conn.query("*", table="mytable", ttl="10m").execute()
-        st.write(rows)
+        ##rows = conn.query("*", table="mytable", ttl="10m").execute()
+        ##st.write(rows)
         ##comando = f"""SELECT * FROM caninos"""
         ##cursor.execute(comando)
         ##resultado = cursor.fetchall()
