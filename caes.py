00e97f7d77a735f0311295b8fe7f1ee1602c52ee
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-10-23 15:31:13 -0400

    Update caes.py

--- caes.py
+++ caes.py
@@ -88,24 +88,24 @@ elif selecao == "ATUALIZAR":
     
 elif selecao == "REMOVER":
     @st.cache_resource
-def init_connection():
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
-    return create_client(url, key)
+    def init_connection():
+        url = st.secrets["SUPABASE_URL"]
+        key = st.secrets["SUPABASE_KEY"]
+        return create_client(url, key)
 
-supabase = init_connection()
+    supabase = init_connection()
 
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query():
-    return supabase.table("mytable").select("*").execute()
+    # Perform query.
+    # Uses st.cache_data to only rerun when the query changes or after 10 min.
+    @st.cache_data(ttl=600)
+    def run_query():
+        return supabase.table("mytable").select("*").execute()
 
-rows = run_query()
+    rows = run_query()
 
-# Print results.
-for row in rows.data:
-    st.write(f"{row['name']} has a :{row['pet']}:")
+    # Print results.
+    for row in rows.data:
+        st.write(f"{row['name']} has a :{row['pet']}:")
     
     st.text("Este modulo ainda está em construção")
     
