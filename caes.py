629996bb8a00c7f8740b5c46ea6ba84beec13c13
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-11-24 15:45:35 -0400

    Update caes.py

--- caes.py
+++ caes.py
@@ -21,8 +21,7 @@ def apresenta():
         comando = f"""SELECT * FROM caninos WHERE genero='macho'"""
         cursor.execute(comando)
         resultado = cursor.fetchall()
-        st.markdown(":sunglasses: O numero de machos é : ")
-        st.markdown(len(resultado))
+        st.markdown(":sunglasses: O numero de machos é : "+str(len(resultado)))
 
         comando = f"""SELECT * FROM caninos WHERE genero='femeas'"""
         cursor.execute(comando)
