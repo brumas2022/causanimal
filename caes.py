374e2a2bf295a728063b7c535393972afbb97702
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-11-24 16:06:44 -0400

    Update caes.py

--- caes.py
+++ caes.py
@@ -112,7 +112,7 @@ def consulta():
         
     cols = st.columns((0.75,0.25))
     ##a = cols[1].markdown(resultado[1])
-    b = cols[1].markdown("Nome :"+resultado[2])
+    b = cols[1].markdown("Nome : "+resultado[2])
     c = cols[1].markdown("Genero : "+resultado[3])
     d = cols[0].image(resultado[12])
     e = cols[1].markdown("Historia :"+resultado[13])
