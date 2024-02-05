b607ae742b2d93f78e06d8e6f0cf19032b0ba0bf
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-02-05 16:22:28 -0400

    Update caes.py

--- caes.py
+++ caes.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Caes abrigados")
 password=0
 while password<10:
    login = st.sidebar.text_input("Digite o login")
-   password = st.sidebar.number_input("digite a senha", value=None)
+   password = st.sidebar.number_input("digite a senha", value=int)
 
 if password=="Shit":
    
